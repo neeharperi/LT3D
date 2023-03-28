85c8e0258eda5bee89b28308fbd7852b30d688df
Neehar Peri <neeharperi@users.noreply.github.com>
2023-03-28 13:39:14 -0400

    Delete scripts directory

--- scripts/LAMBDA-T6.sh
+++
@@ -1,3 +0,0 @@
-#python tools/create_data.py av2 --root-path /ssd0/nperi/Sensor/ --out-dir /ssd0/nperi/Sensor/av2_mmdet3d_trainval/ --max-sweeps 5 --extra-tag av2
-bash tools/dist_train.sh configs/pointpillars/mrp/hv_pointpillars_0125voxel_fpn_sbn-all_4x8_2x_100m_av2.py 8 --no-validate
-bash tools/dist_test.sh configs/pointpillars/mrp/hv_pointpillars_0125voxel_fpn_sbn-all_4x8_2x_100m_av2.py work_dirs/hv_pointpillars_0125voxel_fpn_sbn-all_4x8_2x_100m_av2/latest.pth 8 --out work_dirs/hv_pointpillars_0125voxel_fpn_sbn-all_4x8_2x_100m_av2/predictions.pkl --eval mAP --metric_type standard
