4238540222799c2171d7dcb4947d20f2f78510a9
Neehar Peri <neeharperi@users.noreply.github.com>
2023-03-28 15:52:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 source ~/.bashrc
-conda create -n mmdet3d python=3.8
-conda activate mmdet3d
 
 conda install -y -c anaconda cmake
 pip install torch==1.10.1+cu111 torchvision==0.11.2+cu111 torchaudio==0.10.1 -f https://download.pytorch.org/whl/torch_stable.html
